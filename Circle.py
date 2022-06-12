8e8079a8e9f0585d3404090e615ca5cd845c736e
Xer0n3 - RussC <developing821@outlook.com>
2022-06-12 23:14:10 +0100

    Line ending changes
    from LF to CRLF

---
+++ Circle.py
@@ -0,0 +1,31 @@
+class Circle(float **kwargs):
+	def circle__init__(self, **kwargs):
+		self.X == 0
+		self.Y == 0
+		self.Z == 0
+		self.n_x < X
+		self.n_y < Y
+		self.n_z < Z
+		self.p_x > X
+		self.p_y > Y
+		self.p_z > Z
+		self._PI_ = 3.141592653589793
+		self.rad
+		self.dia
+			if rad and dia:
+				print('ERROR_RADIUS_AND_DIAMETER_SUPPLIED')
+			elif rad:
+				for n in (n < X or n > X):
+					circle = (_PI_ * pow(rad, 2))
+				for n in (n < Y or n > Y):
+					circle = (_PI_ * pow(rad, 2))
+				for n in (n < Z or n > Z):
+					circle = (_PI_ * pow(rad, 2))
+			else:
+				for n in (n < X or n > X):
+					circle = (_PI_ * pow((dia / 2), 2))
+				for n in (n < Y or n > Y):
+					circle = (_PI_ * pow((dia / 2), 2)
+				for n in (n < Z or n > Z):
+					circle = (_PI_ * pow((dia / 2), 2)
+	return circle
